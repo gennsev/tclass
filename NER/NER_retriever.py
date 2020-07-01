9485cc98c686eac18352e0fa2a47eab7b3855df3
Fellipe Franco Couto <65303424+FellipeFrancoCouto@users.noreply.github.com>
2020-07-01 17:25:03 -0300

    NER Class + POS Class Updates

--- NER/NER_retriever.py
+++ NER/NER_retriever.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import spacy
 import numpy as np 
 import pandas as pd
+from spacy.pipeline import EntityRecognizer
 
 nlp = spacy.load("en_core_web_md")
 
-
-class POS_retriever:
+class NER_retriever:
     def fit(self, doc):
         pass
 
-    def POS(self, data):
+    def NER(self, text):
         lst=[]
-        doc = nlp(data)
-        for token in doc:
-            lst.append(token.pos_)
+        doc = nlp(text) 
+        for ent in doc.ents:
+            lst.append(ent.label_)
         return lst
+
+
+# In[ ]:
+
+
+
 
