72e2ffc5c2a8f16f66b33abbcc6fd6d6fc892ac5
Fellipe Franco Couto <65303424+FellipeFrancoCouto@users.noreply.github.com>
2020-07-20 14:17:08 -0300

    Updating fasttext class: adding tester feature

--- _fastText_/fasttext_class.py
+++ _fastText_/fasttext_class.py
@@ -1,4 +1,6 @@
 import fasttext
+import os
+import pandas as pd
 class fasttext_c:
     def __init__(self, load_model=None, train_data=None, epoch=40, lr=1): 
         if load_model is not None:
@@ -10,8 +12,39 @@ class fasttext_c:
                 self.model = fasttext.train_supervised(input=train_data, epoch=epoch, lr=lr)
 
     def fasttext_labeler(self, text):
-        pred_label = model.predict(text)
+        pred_label = self.model.predict(text)
         return pred_label
     
     def save_model(self, path):
         self.model.save_model(str(path))
+
+    def gen_training(self, path, label, outfile_path):
+        """
+        Parameters:
+            path:
+            label:
+            outfile_path:
+             
+        """   
+        rows_list = []
+        for filename in os.listdir(path):
+            with open(f'{path}/{filename}', encoding='utf-8') as f:
+                text = f.read()
+                text_composed = text.replace('\n', ' ')
+                ft_label = '__label__' + str(label)
+                ft_content = str(text_composed) 
+                dict1 = {
+                    'label'   : ft_label,
+                    'content' : ft_content
+                 }
+
+            rows_list.append(dict1)
+            
+        trainingDf = pd.DataFrame(rows_list)   
+        trainingDf.to_csv(outfile_path, header=False, index=False, sep='\t')
+        return trainingDf.head()
+    
+    def fasttext_test(self, testfilepath):
+        test_result = self.model.test(testfilepath)
+        return test_result
+    
